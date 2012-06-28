85e6950b41ff61f6b96c24406d313187170ea0b5
Miguel Bejar <bejar37@gmail.com>
2012-06-28 14:11:45 -0400

    fixed form issue

--- serv.js
+++ serv.js
@@ -3,46 +3,48 @@ var qs = require('qs');
 
 var s  = http.createServer(function(req, res) {
   var msgs = [];
-	req.content = "";
-	var gen_messages = function(msgs){
-		var body = '<html>'+
-		'<head>'+
-		'<meta http-equiv="Content-Type" content="text/html; '+
-		'charset=UTF-8" />'+
-		'</head>'+
-		'<body>'+
-		'<form action="/" method="post">'+
-		'<input placeholder="Name" id="name"/>'+
-		'<input placeholder="Message" id="message"/>'+
-		'<button type="submit" id="submit">Send</button>';
-		for (var i = 0; i<msgs.length; i++){
-			body += '<div class="sender_name">' + msgs.from + '</div>';
-			body += '<div class="chat_message">' + msgs.text + '</div>';
-		}
-		body +='</form>'+
-		'</body>'+
-		'</html>';
+  var content = '';
+  var gen_messages = function(msgs){
+    var body = '<html>'+
+      '<head>'+
+      '<meta http-equiv="Content-Type" content="text/html; '+
+      'charset=UTF-8" />'+
+      '</head>'+
+      '<body>'+
+      '<form action="/" method="post" name="msg">'+
+      '<input name="from" placeholder="Name" id="name"/>'+
+      '<input name="message" placeholder="Message" id="message"/>'+
+      '<button type="submit" id="submit">Send</button>';
+    for (var i = 0; i<msgs.length; i++){
+      body += '<div class="sender_name">' + msgs[i].from + '</div>';
+      body += '<div class="chat_message">' + msgs[i].text + '</div>';
+    }
+    body +='</form>'+
+      '</body>'+
+      '</html>';
 
-		return body;
-	};
+    return body;
+  };
 
 
-if (req.method == 'GET') {
-					res.write(gen_messages(msgs));
-	}
-if (req.method == 'POST') {
-				req.addListener('data', function(data) {
-												req.content += data;
-												});
+  if (req.method == 'POST') {
+    req.on('data', function(data) {
+      content += data;
+      console.log(data);
+    });
 
-				req.addListener('end', function(data) {
-												req.content = qs.parse(req.content);
-												msgs.push(req.content.msg)
-												});
+    req.on('end', function() {
+      console.log('Body: ' + req.body);
+      console.log('Content: ' + content);
+      req.body = JSON.parse(content);
+      msgs.push(req.body.msg);
+      console.log(content);
+    });
 
-}
+  }
 
-res.end();
+  res.write(gen_messages(msgs));
+  res.end();
 
 });
 
