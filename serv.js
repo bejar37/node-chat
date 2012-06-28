807d89ee51f3e7d0cc32ea9b8daeabeb4e0e18d6
Aman Kapur <amankapur91@gmail.com>
2012-06-28 15:26:32 -0400

    add list to this

--- serv.js
+++ serv.js
@@ -4,30 +4,8 @@ var qs = require('qs');
 var msgs = [];
 var s  = http.createServer(function(req, res) {
   console.log('request starting: '+ req.method);
-  var content = '';
-  var gen_messages = function(msgs){
-    var body = '<html>'+
-      '<head>'+
-      '<meta http-equiv="Content-Type" content="text/html; '+
-      'charset=UTF-8" />'+
-      '</head>'+
-      '<body>'+
-      '<form action="/" method="post" name="msg">'+
-      '<input name="from" placeholder="Name" id="name"/>'+
-      '<input name="text" placeholder="Message" id="message"/>'+
-      '<button type="submit" id="submit">Send</button>'+ '</form>' +
-      '<ul>';
-
-    for (var i = 0; i<msgs.length; i++){
-      body += '<li>';
-      body += '<span class="sender_name">From: ' + msgs[i].from + '</span> ';
-      body += '<span class="chat_message">Message: ' + msgs[i].text + '</span> ';
-      body += '</li>';
-    }
-    body += '</ul>' + '</body>'+ '</html>';
-
-    return body;
-  };
+  
+}
 
 
   if (req.method == 'POST') {
@@ -43,7 +21,6 @@ var s  = http.createServer(function(req, res) {
 
   }
 
-  res.write(gen_messages(msgs));
   res.end();
 
 });
