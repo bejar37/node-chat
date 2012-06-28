844148530d0178c75d1e7400e5933df7517626ce
Aman Kapur <amankapur91@gmail.com>
2012-06-28 13:43:56 -0400

    form done

--- serv.js
+++ serv.js
@@ -3,24 +3,46 @@ var qs = require('qs');
 
 var s  = http.createServer(function(req, res) {
   var msgs = [];
-  var req.content = '';
+	req.content = "";
+	var gen_messages = function(msgs){
+		var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/" method="post">'+
+		'<input placeholder="Name" id="name"/>'+
+		'<input placeholder="Message" id="message"/>'+
+		'<button type="submit" id="submit">Send</button>';
+		for (var i = 0; i<msgs.length; i++){
+			body += '<div class="sender_name">' + msgs.from + '</div>';
+			body += '<div class="chat_message">' + msgs.text + '</div>';
+		}
+		body +='</form>'+
+		'</body>'+
+		'</html>';
 
-  req.addListener('data', function(data) {
-    req.content += data;
-  });
-
-  req.addListener('end', function(data) {
-    req.content = qs.parse(req.content);
-  });
-  if (req.method == 'GET') {
-    res.write(gen_messages(msgs));
-  }
-  if (req.method == 'POST') {
-     msgs.push(req.content.msg)
-  }
+		return body;
+	};
 
 
-  res.end();
+if (req.method == 'GET') {
+					res.write(gen_messages(msgs));
+	}
+if (req.method == 'POST') {
+				req.addListener('data', function(data) {
+												req.content += data;
+												});
+
+				req.addListener('end', function(data) {
+												req.content = qs.parse(req.content);
+												msgs.push(req.content.msg)
+												});
+
+}
+
+res.end();
 
 });
 
