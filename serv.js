53f35285532063e3cd7d9e2b7843e5dad8891fad
Aman Kapur <amankapur91@gmail.com>
2012-06-28 18:34:22 -0400

    IT WORKSgit add -Agit add -A

--- serv.js
+++ serv.js
@@ -1,21 +1,43 @@
 var http = require('http');
 var qs = require('qs');
+var url = require('url');
 
 var msgs = [];
+var client_length = 0;
+
 var s  = http.createServer(function(req, res) {
-  console.log('request starting: '+ req.method);
-  
-}
+   
+	var content = "";
 
+	res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
 
-  if (req.method == 'POST') {
-    req.on('data', function(data) {
-      content += data;
+	console.log(req.url);
+	client_length = qs.parse(url.parse(req.url).query).client_length;
+
+	console.log('request starting: '+ req.method);
+	console.log('client length: ' + client_length);
+	console.log('Msg length: ' + msgs.length);
+	
+
+	var	new_msgs = msgs.slice(client_length, msgs.length);
+	
+	var main_object = { 
+			server_length: msgs.length,
+			messages: new_msgs}
+	
+	
+	res.write(JSON.stringify(main_object));
+	
+
+  	if (req.method == 'POST') {
+    	req.on('data', function(data) {
+    	content += data;
     });
 
     req.on('end', function() {
       req.body = qs.parse(content);
       var msg = { from: req.body.from, text: req.body.text };
+	  if(req.method == 'POST'){ client_length = req.body.client_length;}
       msgs.push(msg);
     });
 
@@ -25,4 +47,4 @@ var s  = http.createServer(function(req, res) {
 
 });
 
-s.listen(8000);
+s.listen(3000);
